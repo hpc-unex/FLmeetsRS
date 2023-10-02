c1d39a20c93300ac9ef060fce66925b1df983a1e
Sergio Moreno-Álvarez <36038967+hpc-unex@users.noreply.github.com>
2023-10-02 17:40:55 +0200

    Update rsicb256 execute_patch.sh

--- patch/execute_patch.sh
+++ patch/execute_patch.sh
@@ -9,6 +9,7 @@ cp -r data_loader.py FedML/python/fedml/data/
 cp -r arguments.py FedML/fedml/
 cp -r __init__py FedML/fedml/
 cp -r RSICB256 FedML/python/fedml/data
+mkdir ~/FLmeetsRS/FedML/fedml_data/RSICB256
 
 cd ../FedML
 pip install fedml
