c013ee39a9e863aaf8b5c15d9cca7f9f781f0f58
Sergio Moreno-Álvarez <36038967+hpc-unex@users.noreply.github.com>
2023-10-02 18:01:14 +0200

    Update execute_patch.sh

--- patch/execute_patch.sh
+++ patch/execute_patch.sh
@@ -1,14 +1,14 @@
 import os
 
 cd patch
-cp -r yamls FedML/python/
-cp -r main.py FedML/python/
-cp -r launchMPI.sh FedML/python/
-cp -r algorithms FedML/python/
-cp -r data_loader.py FedML/python/fedml/data/
-cp -r arguments.py FedML/fedml/
-cp -r __init__py FedML/fedml/
-cp -r RSICB256 FedML/python/fedml/data
+cp -r yamls ../FedML/python/
+cp -r main.py ../FedML/python/
+cp -r launchMPI.sh ../FedML/python/
+cp -r algorithms ../FedML/python/
+cp -r data_loader.py ../FedML/python/fedml/data/
+cp -r arguments.py ../FedML/fedml/
+cp -r __init__py ../FedML/fedml/
+cp -r RSICB256 ../FedML/python/fedml/data
 mkdir ~/FLmeetsRS/FedML/fedml_data/RSICB256
 
 cd ../FedML
